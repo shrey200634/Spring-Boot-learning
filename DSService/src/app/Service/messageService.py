a4cd6860f1d159e8ff5d9b304694585c3fe905fe
shrey200634 <dshrey60@gmail.com>
2026-03-07 16:46:38 +0530

    complete ds service . kafka not intregrated .

--- DSService/src/app/Service/messageService.py
+++ DSService/src/app/Service/messageService.py
@@ -5,5 +5,11 @@ from Service.LLMService import LLMService
 
 class MessageService:
     def __init__(self):
-        self.messageUtil =MessagesUtil();
-        self.llmService = LLMService();
+        self.messageUtil =MessagesUtil()
+        self.llmService = LLMService()
+
+    def process_message(self , message ):
+        if self.messageUtil.isBankSms(message):
+            return self.llmService.runLLM(message)
+        else:
+            return None
--- DSService/src/app/__init__.py
+++ DSService/src/app/__init__.py
@@ -1,11 +1,22 @@
 from flask import Flask, request, jsonify
-from .Service import messageService 
+from Service.messageService import messageService 
+
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
+
+messageService =messageService()
 
 @app.route('/v1/ds/message/', methods=['POST'])
 def handle_message():
     message = request.json.get('message')
     result = messageService.process_message(message)
     return jsonify(result)
+
+@app.route('/', methods=['GET'])
+def handle_get():
+    print("Hello World")
+
+if __name__ == "__main__":
+    app.run(host="localhost" , port=8080 , debug=True)
+
